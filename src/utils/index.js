4834b38b63a20970c48c25abb87ea6a841544315
Elon Salfati <elon@salfati.io>
2020-11-29 14:34:05 +0200

    added format util function that converts json to struct json for rest api

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,67 @@
+/**
+ * Formats list value.
+ * 
+ * @param {Array} values - List values to format.
+ */
+const formatList = (values) => {
+    let ret = {
+        listValue: {
+            values: []
+        }
+    }
+
+    // iterate over the list and format the values
+    for (const item of values) {
+        ret.listValue.values.push(
+            formatValue(item)
+        )
+    }
+
+    return ret
+}
+
+/**
+ * Format custom value.
+ * 
+ * @param {*} value - Value to format
+ */
+const formatValue = (value) => {
+    let ret = {}
+
+    if (value === null || value === undefined) {
+        ret = {
+            "nullValue": null
+        }
+    } else {
+        switch (value.constructor.name.toLowerCase()) {
+            case "string":
+                ret = {
+                    "stringValue": value
+                }
+                break
+            case "number":
+                ret = {
+                    "numberValue": value
+                }
+                break
+            case "boolean":
+                ret = {
+                    "boolValue": value
+                }
+                break
+            case "array":
+                ret = formatList(value)
+                break
+            case "object":
+                ret = this.format(value)
+                break
+            default:
+                throw new Error("unexpected struct type")
+        }
+    }
+
+    return ret
+}
 
 /**
  * Format the payload data according to the requirements
@@ -6,5 +70,16 @@
  * @param {object} payload - Object to format.
  */
 module.exports.format = (payload) => {
-    return payload
+    let ret = {
+        structValue: {
+            fields: {}
+        }
+    }
+
+    // iterate over the keys and append value
+    for (const [property, value] of Object.entries(payload)) {
+        ret.structValue.fields[property] = formatValue(value)
+    }
+
+    return ret
 }
