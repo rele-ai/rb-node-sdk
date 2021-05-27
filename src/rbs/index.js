e1da75e0bb981e91c3ee28e8543693f7da9fba13
GalShalom <44848577+Galshalom5@users.noreply.github.com>
2021-05-27 14:07:43 +0300

    generate on 'scripts/build-proto.sh' required

--- src/rbs/index.js
+++ src/rbs/index.js
@@ -9,28 +9,29 @@ const { notifyHandler } = require("./handlers")
 const { IntegratedAppsService } = require("../pb/integratedapp_grpc_pb")
 
 /**
-* RBS - Releai Bot Server
-*
-* Helps you to create a simple integrated application
-* with rele.ai.
-*/
+ * RBS - Releai Bot Server
+ *
+ * Helps you to create a simple integrated application
+ * with rele.ai.
+ */
 class RBS {
   /**
-  * Initiate the releai bot server instance.
-  *
-  * @param {object} confs - Server configurations.
-  */
+   * Initiate the releai bot server instance.
+   *
+   * @param {object} confs - Server configurations.
+   */
   constructor(confs) {
     // get full server configurations
     this._confs = this._getFullConfs(confs)
 
     // check app id and app hash
     if (!this._confs.appId) throw new Error(`APP_ID is a required attribute`)
-    if (!this._confs.appHash) throw new Error(`APP_HASH is a required attribute`)
+    if (!this._confs.appHash)
+      throw new Error(`APP_HASH is a required attribute`)
 
     logger.debug({
       message: "initialize RBS with user confs",
-      payload: confs
+      payload: confs,
     })
 
     // init operations object
@@ -38,8 +39,8 @@ class RBS {
   }
 
   /**
-  * Initiate the gRPC server and handler
-  */
+   * Initiate the gRPC server and handler
+   */
   _initGrpcServer() {
     // init the server
     this._server = new grpc.Server(this._confs.grpc.serverInit)
@@ -51,51 +52,51 @@ class RBS {
       IntegratedAppsService,
 
       // register the handler
-      notifyHandler(this._operations, this._confs),
+      notifyHandler(this._operations, this._confs)
     )
     logger.debug({ message: "initiated notification handler" })
   }
 
   /**
-  * Returns a full version of the server configurations
-  * contact to the user configs.
-  *
-  * @param {object} userConfs - The user configurations.
-  * @returns {object} - Server configurations.
-  */
+   * Returns a full version of the server configurations
+   * contact to the user configs.
+   *
+   * @param {object} userConfs - The user configurations.
+   * @returns {object} - Server configurations.
+   */
   _getFullConfs(userConfs) {
     return {
       // default settings
       grpc: {
         serverInit: {
           "grpc.max_receive_message_length": 30000000,
-          "grpc.max_send_message_length": 30000000
+          "grpc.max_send_message_length": 30000000,
         },
         creds: grpc.ServerCredentials.createInsecure(),
       },
       middlewares: middlewares,
 
       // override with user confs
-      ...userConfs
+      ...userConfs,
     }
   }
 
   /**
-  * Register a new handler for the application.
-  *
-  * @param {string} operationKey - The operation key name.
-  * @param {Function} callback - The handler for the request.
-  */
+   * Register a new handler for the application.
+   *
+   * @param {string} operationKey - The operation key name.
+   * @param {Function} callback - The handler for the request.
+   */
   registerOperation(operationKey, callback) {
     logger.info({ message: "registering operation handler", operationKey })
     this._operations[operationKey] = callback
   }
 
   /**
-  * Register the provided handlers.
-  *
-  * @param {object} handlers - Handlers object.
-  */
+   * Register the provided handlers.
+   *
+   * @param {object} handlers - Handlers object.
+   */
   use(handlers) {
     // check if handlers object is workable
     if (handlers) {
@@ -105,10 +106,14 @@ class RBS {
       if (handlers.constructor === Router) {
         routes = handlers.routes
       } else if (handlers.constructor === Object) {
-        console.warn("Direct usage of objects as routes will be deprecated. Please use the Router object instead.")
+        console.warn(
+          "Direct usage of objects as routes will be deprecated. Please use the Router object instead."
+        )
         routes = handlers
       } else {
-        throw new Error("handlers object must be of type Router or Object(deprecated)")
+        throw new Error(
+          "handlers object must be of type Router or Object(deprecated)"
+        )
       }
 
       // iterate over the module and register the handlers
@@ -116,7 +121,9 @@ class RBS {
         this.registerOperation(operationKey, callback)
       }
     } else {
-      throw new Error("handlers object must be of type Router or object (deprecated).")
+      throw new Error(
+        "handlers object must be of type Router or object (deprecated)."
+      )
     }
   }
 
@@ -137,13 +144,13 @@ class RBS {
   }
 
   /**
-  * Sign and start the gRPC server on the provided
-  * host:port.
-  *
-  * @param {number} port - The port number.
-  * @param {string} [host=0.0.0.0] - The target host.
-  */
-  listen(port, host="0.0.0.0") {
+   * Sign and start the gRPC server on the provided
+   * host:port.
+   *
+   * @param {number} port - The port number.
+   * @param {string} [host=0.0.0.0] - The target host.
+   */
+  listen(port, host = "0.0.0.0") {
     // required for rb dev script - export port and host
     this._shareAddr(host, port)
 
@@ -151,13 +158,22 @@ class RBS {
     this._initGrpcServer()
 
     // bind to host:port
-    this._server.bind(`${host}:${port}`, this._confs.grpc.creds)
+    this._server.bindAsync(
+      `${host}:${port}`,
+      this._confs.grpc.creds,
+      (err, port) => {
+        if (err != null) {
+          logger.info(`error while trying to bind on port :${port}`)
+        } else {
+          logger.info(`server bind sucsses using port:${port}`)
+          // listen on port
+          this._server.start()
 
-    // listen on port
-    this._server.start()
-
-    // log
-    logger.info({ message: "server running", port, host })
+          // log
+          logger.info({ message: "server running", port, host })
+        }
+      }
+    )
   }
 }
 
